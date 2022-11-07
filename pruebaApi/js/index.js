5fe1e0376ef1cacdc3a182ea08bd5994632454c2
Juan Pablo <juan.ospina.vanegas@correounivalle.edu.co>
2022-11-07 14:19:16 -0500

    Se consume la api y se muestra en pantalla

---
+++ pruebaApi/js/index.js
@@ -0,0 +1,52 @@
+const $form = document.querySelector("form");
+const $book = document.getElementById("book");
+const $author = document.getElementById("author");
+const $next = document.getElementById('btn-next');
+const $back = document.getElementById('btn-back');
+const LIMIT = 8;
+let offset = 0;
+
+let api = 'https://openlibrary.org/search.json?';
+
+$form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const regex = / /gi;
+  if ($book.value && $author.value) {
+    const book = $book.value.trim().replace(regex, "+");
+    const author = $author.value.trim().replace(regex, "+");
+    api += `title=${book}?author=${author}`;
+  } else if ($book.value) {
+    const book = $book.value.trim().replace(regex, "+");
+    api += `title=${book}`;
+  } else if ($author.value) {
+    const author = $author.value.trim().replace(regex, "+");
+    api += `author=${author}`;
+  }
+  httpFetch();
+});
+
+function httpFetch() {
+  fetch(`${api}&offset=${offset}&limit=${LIMIT}`)
+    .then((res) => res.json())
+    .then((data) => {
+      let $containerInfo = document.querySelector(".container-info");
+      if (data.docs.length > 0) {
+        let html = '';
+        for (let i = 0; i < data.docs.length; i++) {
+          html += `
+          <h2>${data.docs[i].title}</h2>
+          <h3>${data.docs[i].author_name}</h3>
+          <img src='http://covers.openlibrary.org/b/isbn/${data.docs[i].isbn[0]}-M.jpg'/>`;
+        }
+        console.log(html)
+        $containerInfo.innerHTML = html;
+      } else {
+        $containerInfo.innerHTML = "<h3>Not found</h3>";
+      }
+    });
+}
+
+$next.onclick = (e) => {
+  offset += 8;
+  httpFetch();
+}
